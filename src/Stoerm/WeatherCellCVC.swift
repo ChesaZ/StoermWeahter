984b3d52e0a98a01dc77a04c5b0245a2f138fdd7
Jonathan G <ChesaZ@users.noreply.github.com>
2017-08-10 13:44:35 +0300

    Create WeatherCellCVC.swift

---
+++ src/Stoerm/WeatherCellCVC.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherCellCVC.swift
+//  StoermWeather
+//
+//  Created by Jonas Gamburg on 04/08/2017.
+//  Copyright © 2017 Jonas Gamburg. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCellCVC: UICollectionViewCell {
+    
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    @IBOutlet weak var dayOfWeekLabel: UILabel!
+    
+    
+}
