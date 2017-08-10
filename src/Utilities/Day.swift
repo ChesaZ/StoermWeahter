afd2a34018b34963de7e8bbba23cfbb07ec89072
Jonathan G <ChesaZ@users.noreply.github.com>
2017-08-10 13:35:39 +0300

    Create Day.swift

---
+++ src/Utilities/Day.swift
@@ -0,0 +1,21 @@
+//
+//  Day.swift
+//  StoermWeather
+//
+//  Created by Jonas Gamburg on 06/08/2017.
+//  Copyright © 2017 Jonas Gamburg. All rights reserved.
+//
+
+import Foundation
+
+struct Day {
+    let day: String
+    let maxTemp: String
+    let maxFeels: String
+    let windSpeed: String
+    let sunrise: String
+    let sunset: String
+    let humidity: String
+    let precipProbability: String
+    let summary: String
+}
